570d5c16eb0565ed20fa4ba8c60f1c071ad56f86
Eric Monti <emonti@matasano.com>
2009-09-12 20:44:27 -0500

    doc and samples cleanup

--- samples/basic.rb
+++ samples/basic.rb
@@ -1,35 +1,55 @@
-require 'rubygems'
-require 'ffi'
-require 'ffi/dry'
+#!/usr/bin/env ruby
+# One major feature is a dsl"-like" syntax for declaring structure members
+# in FFI::Struct or FFI::ManagedStruct definitions.
+    
+    require 'rubygems'
+    require 'ffi'
+    require 'ffi/dry'
 
-class SomeStruct < FFI::Struct
-  include FFI::DRY::StructHelper
+    class SomeStruct < FFI::Struct
+      include FFI::DRY::StructHelper
 
-  # we get a new way of specifying layouts with a 'dsl'-like syntax
-  dsl_layout do
-    field   :field1,  :uint16, :desc => 'this is field 1'
-    field   :field2,  :uint16, :desc => 'this is field 2'
-  end
-end
+      # we get a new way of specifying layouts with a 'dsl'-like syntax
+      # The hash containing {:desc => ... } can contain arbitrary keys which 
+      # can be used however we like. dsl_metadata will contain all these
+      # in the class and instance.
+      dsl_layout do
+        field   :field1,  :uint16, :desc => 'this is field 1'
+        field   :field2,  :uint16, :desc => 'this is field 2'
+      end
+    end
+
+    ss0=SomeStruct.new
+
+# With the declaration above, we specified :desc hash value, which was stored
+# in metadata along with the field name and type.
+
+    pp ss0.dsl_metadata 
+    [{:type=>:uint16, :name=>:field1, :desc=>"this is field 1"},
+     {:type=>:uint16, :name=>:field2, :desc=>"this is field 2"}]
+    # => nil
 
 
-ss0=SomeStruct.new
+# We get some free additional ways of instantiating and declaring values during 
+# initialization. (The FFI standard ways still work too)
 
-p ss0.dsl_metadata   # we can look at definition metadata
+    raw_data = "\x00\x00\xff\xff"
 
-# And we have additional ways of instantiating and declaring values
-# during initialization. (The FFI standard ways still work too)
+    ss1=SomeStruct.new :raw => raw_data
+    ss2=SomeStruct.new :raw => raw_data, :field1 => 1, :field2 => 2
+    ss3=SomeStruct.new {|x| x.field1=1 }
+    ss4=SomeStruct.new(:raw => raw_data) {|x| x.field1=1 }
 
-raw_data = "\x00\x00\xff\xff"
+    [ ss0, 
+      ss1, 
+      ss2, 
+      ss3, 
+      ss4 ].each_with_index {|x,i| p ["ss#{i}",[x.field1, x.field2]]}
 
-ss1=SomeStruct.new :raw => raw_data
-ss2=SomeStruct.new :raw => raw_data, :field1 => 1, :field2 => 2
-ss3=SomeStruct.new {|x| x.field1=1 }
-ss4=SomeStruct.new(:raw => raw_data) {|x| x.field1=1 }
-
-[ ss0, 
-  ss1, 
-  ss2, 
-  ss3, 
-  ss4].each_with_index {|x,i| p ["ss#{i}",[x.field1, x.field2]]}
+# which should produce...
+# ["ss0", [0, 0]]
+# ["ss1", [0, 65535]]
+# ["ss2", [1, 2]]
+# ["ss3", [1, 0]]
+# ["ss4", [1, 65535]]
 
--- samples/describer.rb
+++ samples/describer.rb
@@ -1,48 +1,119 @@
-require 'rubygems'
-require 'ffi'
-require 'ffi/dry'
+# Here's a broader example which utilizes that arbitrary ':desc' parameter in a 
+# "neighborly" way. This also demonstrates superclasses to add common struct
+# features, declaring array fields, as well as nesting other structs.
 
-class NeighborlyStruct < ::FFI::Struct
-  include ::FFI::DRY::StructHelper
+    require 'rubygems'
+    require 'ffi'
+    require 'ffi/dry'
 
-  def self.describe
-    print "Struct: #{self.class}"
-    dsl_metadata().each_with_index do |spec, i|
-      print "  Field #{i}\n"+
-            "    name:  #{spec[:name].inspect}\n"+
-            "    type:  #{spec[:type].inspect}\n"+
-            "    desc:  #{spec[:desc]}\n\n"
+    class NeighborlyStruct < ::FFI::Struct
+      include ::FFI::DRY::StructHelper
+
+      def self.describe
+        print "Struct: #{self.name}"
+        dsl_metadata().each_with_index do |spec, i|
+          print "  Field #{i}\n"
+          print "    name:  #{spec[:name].inspect}\n"
+          print "    type:  #{spec[:type].inspect}\n"
+          print "    desc:  #{spec[:desc]}\n\n"
+        end
+        print "\n"
+      end
+      def describe;  self.class.describe;  end
     end
-    print "\n"
-  end
-  def describe;  self.class.describe;  end
-end
 
-class TestStruct < NeighborlyStruct
-  dsl_layout do
-    field   :field1,  :uint8,  :desc => "test field 1"
-    field   :field2,  :uint8,  :desc => "test field 2"
-  end
-end
+    class TestStruct < NeighborlyStruct
+      dsl_layout do
+        field   :field1,  :uint8,  :desc => "test field 1"
+        field   :field2,  :uint8,  :desc => "test field 2"
+      end
+    end
 
-class SomeStruct < NeighborlyStruct
-  dsl_layout do
-    field  :kind, :uint8,      :desc => "a type identifier"
-    struct :tst,  TestStruct,  :desc => "a nested TestStruct"
-    field  :len,  :uint8,      :desc => "8-bit size value (>= self.size+2)"
-    array  :str,  [:char,255], :desc => "a string up to 255 bytes bound by :len"
-  end
+    class SomeStruct < NeighborlyStruct
+      dsl_layout do
+        field  :kind, :uint8,      :desc => "a type identifier"
+        struct :tst,  TestStruct,  :desc => "a nested TestStruct"
+        field  :len,  :uint8,      :desc => "8-bit size value (>= self.size+2)"
+        array  :str,  [:char,255], 
+            :desc => "a string up to 255 bytes bound by :len"
+      end
 
-  # override kind getter method
-  def kind
-    [:default, :bar, :baz][ self[:kind] ]
-  end
-end
+      # override kind getter method with our own
+      # resolves kind to some kind of type array for example...
+      def kind
+        [:default, :bar, :baz][ self[:kind] ]
+      end
+    end
 
-s1=TestStruct.new
-s2=SomeStruct.new
+    s1=TestStruct.new
+    s2=SomeStruct.new
 
-puts "*"*70
-s1.describe
-puts "*"*70
-s2.describe
+    # check out that 'kind' override:
+    s2.kind
+    # => :default
+
+    # oh and the regular FFI way is always intact
+    s2[:kind]
+    # => 0
+
+    s2[:kind]=1
+    s2.kind
+    # => :bar
+
+    s2.kind=3
+    s2.kind
+    # => :baz
+
+    puts "*"*70
+    s1.describe
+    ## we get a dump of metadata
+    # **********************************************************************
+    # Struct: TestStruct  
+    # Field 0
+    #   name:  :field1
+    #   type:  :uint8
+    #   desc:  test field 1
+    #
+    # Field 1
+    #   name:  :field2
+    #   type:  :uint8
+    #   desc:  test field 2
+
+    puts "*"*70
+    s2.describe
+    ## we get a dump of metadata
+    # Struct: SomeStruct  Field 0
+    #     name:  :kind
+    #     type:  :uint8
+    #     desc:  a type identifier
+    #
+    #   Field 1
+    #     name:  :tst
+    #     type:  TestStruct
+    #     desc:  a nested TestStruct
+    #
+    #   Field 2
+    #     name:  :len
+    #     type:  :uint8
+    #     desc:  8-bit size value (>= self.size+2)
+    #
+    #   Field 3
+    #     name:  :str
+    #     type:  [:char, 255]
+    #     desc:  a string up to 255 bytes bound by :len
+
+    puts "*"*70
+    s2.tst.describe
+    ## same as s1.describe
+    # **********************************************************************
+    # Struct: TestStruct  
+    # Field 0
+    #   name:  :field1
+    #   type:  :uint8
+    #   desc:  test field 1
+    #
+    # Field 1
+    #   name:  :field2
+    #   type:  :uint8
+    #   desc:  test field 2
+
